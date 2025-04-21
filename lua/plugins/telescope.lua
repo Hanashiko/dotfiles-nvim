2f6562bf1e26ff36554ff717fb3f7e573a679449
Hanashiko <hlichisper@gmail.com>
2025-04-22 01:37:11 +0300

    translate comments in telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,18 @@
 
--- Настраиваем комбинации под разные функции
+-- Налаштовуємо комбінації клаві для різних функцій
 local builtin = require('telescope.builtin')
 
--- Работа с файлами и буфферами
+-- Робота з файлами та буферами
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ft', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- Работа с Git
+-- Робота з Git
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
 
--- Выбор цветовой схемы
+-- Вибір кольорової схеми
 vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
 
